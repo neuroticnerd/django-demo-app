1646ba7d5a88813ac7bb0b10abcd9890ab34bc39
neuroticnerd <eggleton.bryce@gmail.com>
2016-02-18 03:32:28 -0500

    Logging settings and error pages

--- django_demo/settings.py
+++ django_demo/settings.py
@@ -20,7 +20,7 @@ def env(key, default=None, coax=str):
     return coax(value)
 
 
-SITE_ID = 1
+SITE_ID = env('SITE_ID', 1, int)
 BASE_DIR = dirname(dirname(abspath(__file__)))
 
 DEBUG = env('DEBUG', True, bool)
@@ -29,7 +29,7 @@ DEBUG = env('DEBUG', True, bool)
 DEFAULT_SECRET_KEY = 'j1^szqq)h*bmvv2p8!j(uzcdn)9f*yvp5vwh_w8@g5_(u@uns)'
 SECRET_KEY = env('SECRET_KEY', DEFAULT_SECRET_KEY)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env('ALLOWED_HOSTS', ['*'], hydrate)
 WSGI_APPLICATION = 'django_demo.wsgi.application'
 ROOT_URLCONF = 'django_demo.urls'
 
@@ -89,15 +89,16 @@ DJANGO_TEMPLATES = {
 }
 
 # use cached template loading for performance when in production
-if DEBUG:
-    DJANGO_TEMPLATES['APP_DIRS'] = True
-else:
+CACHED_TEMPLATES = env('CACHED_TEMPLATES', (not DEBUG), bool)
+if CACHED_TEMPLATES:
     DJANGO_TEMPLATE_OPTIONS['loaders'] = [
         ('django.template.loaders.cached.Loader', [
             'django.template.loaders.filesystem.Loader',
             'django.template.loaders.app_directories.Loader',
         ]),
     ]
+else:
+    DJANGO_TEMPLATES['APP_DIRS'] = True
 
 TEMPLATES = [
     DJANGO_TEMPLATES,
@@ -117,3 +118,43 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
+
+LOGFILE_ROOT = env('LOGFILE_ROOT', BASE_DIR)
+LOGFILE_NAME = env('LOGFILE_NAME', 'django.demo.log')
+LOGFILE_PATH = env('LOGFILE_PATH', join(LOGFILE_ROOT, LOGFILE_NAME))
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': (
+                '[%(asctime)s] %(levelname)s '
+                '[%(pathname)s:%(lineno)s] %(message)s'
+            ),
+            'datefmt': '%d/%b/%Y %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'django_log_file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': LOGFILE_PATH,
+            'formatter': 'verbose'
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'django_log_file'],
+            'propagate': True,
+            'level': 'DEBUG',
+        },
+    }
+}
