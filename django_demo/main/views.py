056e833f4e55d0a03c3b787506a9d74d9ce51fc8
neuroticnerd <eggleton.bryce@gmail.com>
2016-02-19 19:42:32 -0500

    Improved information shown on user dashboard

--- django_demo/main/views.py
+++ django_demo/main/views.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from django.views import generic
 
 from django_demo.accounts.utils import AuthenticatedView
-from django_demo.actions.models import Action
+from django_demo.actions.models import Action, ActionAssignment
 
 
 class LandingView(generic.TemplateView):
@@ -17,11 +17,16 @@ class DashboardView(AuthenticatedView, generic.TemplateView):
     def get_context_data(self, **kwargs):
         context = super(DashboardView, self).get_context_data(**kwargs)
         current_month = datetime.now().month
+        assignments = ActionAssignment.objects.filter(
+            created_at__month=current_month,
+            action__created_at__month=current_month,
+            user=self.request.user
+        ).select_related('action')
+        context['assigned_actions'] = [a.action for a in assignments]
         context['month_actions'] = Action.objects.filter(
             created_at__month=current_month
         )
-        context['my_actions'] = Action.objects.filter(
-            created_at__month=current_month,
+        context['created_actions'] = Action.objects.filter(
             created_by=self.request.user
         )
         return context
